0bf18df9eabbfbfcbce67546712a034d31f7cb08
liannsoo <asoosai@uchicago.edu>
2024-10-21 17:10:35 +0000

    initial

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:form)
+end
+
+get '/process_roll' do
+  dice = params[:dice].to_i
+  sides = params[:sides].to_i
+  rolls = Array.new(dice) { rand(1..sides) }
+  total = rolls.sum
+  erb :result, locals: { rolls: rolls, sides: sides, total: total }
 end
